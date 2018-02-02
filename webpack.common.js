4132f4097c438a98f8eba64b95f672d9f8c567d4
hayden7913 <hayden321@gmail.com>
2018-02-01 19:03:48 -0800

    Adds new wepback config files

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/index.js',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpg|svg|woff|woff2|eot|ttf)$/,
+        exclude: /node_modules/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/public/index.html`,
+    }),
+    new webpack.NamedModulesPlugin(),
+  ],
+};
---
+++ webpack.dev.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /(\.scss$|\.css$)/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  devServer: {
+    overlay: true,
+    port: 3000,
+    stats: 'minimal',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+});
